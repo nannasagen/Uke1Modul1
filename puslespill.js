5c08a9f867514c86a328baf5ceda38ffb5836906
nannasagen <82771340+nannasagen@users.noreply.github.com>
2021-05-26 11:31:27 +0200

    Add files via upload

---
+++ puslespill.js
@@ -0,0 +1,19 @@
+//model
+let appen = document.getElementById("app");
+
+
+
+
+//view
+function view(){
+let html =`
+<div class="brett">
+<div class="box">1</div><div class="box">2</div><div class="box">3</div>
+<div class="box">4</div><div class="box">5</div><div class="box">6</div>
+<div class="box">7</div><div class="box">8</div><div class="box">9</div>
+
+</div>`;
+appen.innerHTML= html;
+}
+
+//controller
