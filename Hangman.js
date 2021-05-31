94822b122c2c50b2994e6a378ebea0a797bc9e98
nannasagen <82771340+nannasagen@users.noreply.github.com>
2021-05-31 14:53:32 +0200

    Add files via upload

---
+++ Hangman.js
@@ -0,0 +1,26 @@
+//Model
+let `buttons
+<div class="buttons">
+<button class="buttons" id="a" onclick="key(this.innerHTML)">A</button><button class="buttons" onclick="key(this.innerHTML)">B</button> 
+    <button class="buttons" onclick="key(this.innerHTML)">C</button><button class="buttons" onclick="key(this.innerHTML)">D</button>
+    <button class="buttons" onclick="key(this.innerHTML)">E</button><button class="buttons" onclick="key(this.innerHTML)">F</button>
+    <button class="buttons" onclick="key(this.innerHTML)">G</button><button class="buttons" onclick="key(this.innerHTML)">H</button>
+    <button class="buttons" onclick="key(this.innerHTML)">I</button><button class="buttons" onclick="key(this.innerHTML)">J</button>
+    <button class="buttons" onclick="key(this.innerHTML)">K</button><button class="buttons" onclick="key(this.innerHTML)">L</button>
+    <button class="buttons" onclick="key(this.innerHTML)">M</button><button class="buttons" onclick="key(this.innerHTML)">N</button>
+    <button class="buttons" onclick="key(this.innerHTML)">O</button><br><button class="buttons" onclick="key(this.innerHTML)">P</button>
+    <button class="buttons" onclick="key(this.innerHTML)">Q</button><button class="buttons" onclick="key(this.innerHTML)">R</button>
+    <button class="buttons" onclick="key(this.innerHTML)">S</button><button class="buttons" onclick="key(this.innerHTML)">T</button>
+    <button class="buttons" onclick="key(this.innerHTML)">U</button><button class="buttons" onclick="key(this.innerHTML)">V</button>
+    <button class="buttons" onclick="key(this.innerHTML)">W</button><button class="buttons" onclick="key(this.innerHTML)">X</button>
+    <button class="buttons" onclick="key(this.innerHTML)">Y</button><button class="buttons" onclick="key(this.innerHTML)">Z</button>
+    <button class="buttons" onclick="key(this.innerHTML)">Æ</button><button class="buttons" onclick="key(this.innerHTML)">Ø</button>
+    <button class="buttons" onclick="key(this.innerHTML)">Å</button>
+    </div>`;
+//View
+
+
+//Controller
+
+
+
