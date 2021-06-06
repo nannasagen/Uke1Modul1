6594076767f5b62416ce089a47242f8fc9ff5820
nannasagen <82771340+nannasagen@users.noreply.github.com>
2021-06-06 21:22:06 +0200

    Add files via upload

--- Hangman.js
+++ Hangman.js
@@ -1,34 +1,26 @@
 
  //Model
-let answer = "VEKST"
-let guess = [];
-
-
-function key(bokstav) {
-    if (bokstav === 'V' || bokstav === 'E' || bokstav === 'K' || bokstav === 'S' || bokstav === 'T') {
-        bokstav.disabled = true;
-        guess.push(bokstav)
-
-
-
-        updateView();
-        checkWin();
-        return `rikitg`;
-    } else return `feil`;
-
-}
-function checkWin() {
-
-
-    if (guess[0] == 'V' && guess[1] == 'E' && guess[2] == 'K' && guess[3] == 'S' && guess[4] == 'T') {
-        alert('Gratulerer!')
-        return `hallo`;
-    } else return `stå på`;
-}
-
-function hei() {
-    return `hei`;
-}
+ let guess = [];
+ let answer = "VEKST";
+ let html = "";
+ 
+ function key(bokstav) {
+     if (bokstav === 'V' || bokstav === 'E' || bokstav === 'K' || bokstav === 'S' || bokstav === 'T') {
+         bokstav.disabled = true;
+         guess.push(bokstav)
+         updateView();
+         checkWin();
+         return `riktig`;
+     } else return `feil`;
+ }
+ function checkWin() {
+     if (!(guess[0] == 'V' && guess[1] == 'E' && guess[2] == 'K' && guess[3] == 'S' && guess[4] == 'T')) {
+         return `stå på`;
+     } else {
+         alert("Du vant");
+         return "Du vant";
+     };
+ }
     
 
 
